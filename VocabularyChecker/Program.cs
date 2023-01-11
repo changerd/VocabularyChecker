d0f95b8d22e12703a9d4586eaac3a6822326bd41
Slava Morza <bloodreek@gmail.com>
2023-01-12 01:42:06 +0300

    Delete startup

--- VocabularyChecker/Program.cs
+++ VocabularyChecker/Program.cs
@@ -1,8 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using System.Text;
-using VocabularyChecker;
 using VocabularyChecker.Data;
 try
 {
